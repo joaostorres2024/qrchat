9c1b20377f3b32d7286b872bcdb3a98f96af5591
joaostorres2024 <joao.pedrost2004@gmail.com>
2025-02-05 13:54:14 -0300

    Adicionando arquivos vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import tophead from './components/tophead.vue'
+</script>
+
+<template>
+  <tophead></tophead>
+</template>
+
+<style scoped>
+   *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+ }
+</style>
---
+++ src/components/tophead.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <div class="header-container">
+            <img class="logo" src="../assets/logo.png" alt="Logo QR Chat">
+            <h3>QR Chat.io</h3>
+        </div>
+    </header>
+</template>
+
+<style>
+ *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+ }
+</style>
+
+<script>
+
+</script>
