391f18aaf1c91043cfb68487c6ffb21ceefd5239
joaostorres2024 <joao.pedrost2004@gmail.com>
2025-02-05 15:41:08 -0300

    Tela desktop pronta

--- src/components/write.vue
+++ src/components/write.vue
@@ -1,84 +1,134 @@
 <template>
-    <div class="write-container">
-        <input class="number" type="text" placeholder="Your number here">
-        <textarea class="message" placeholder="Write a message"></textarea>
-        <button class="btn-go" type="submit">GO</button>
-    </div>
-    <div class="qr-code-container">
-
+    <div class="input-container">
+        <div class="write-container">
+            <input class="number" type="text" placeholder="Your number here">
+            <textarea class="message" placeholder="Write a message"></textarea>
+            <button class="btn-go" type="submit">GO</button>
+        </div>
+        <div class="qr-code-section">
+            <div class="qr-code-container">
+                <!-- QR Code será inserido aqui -->
+            </div>
+            <div class="btn-download">
+                <button class="btn-download-btn" type="submit">DOWNLOAD</button>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-    .write-container{
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: auto;
-        width: 100vw;
-        display: flex;
-    }
+    .write-container {
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    display: flex;
+    height: 100%;
+    width: auto;
+}
 
-    ::placeholder{
-        color: #8f8f8f;
-    }
+.qr-code-section {
+    margin-right: 40px;
+    margin-left: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20px;
+}
 
-    .number{
-        margin: 20px 0;
-        font-family: 'Poppins';
-        outline: #FCFCFC;
-        border: none;
-        border-radius: 10px;
-        background-color: #DAF3EA;
-        padding: 15px;
-        font-size: 20px;
-        height: 60px;
-        width: 400px;
-    }
+.btn-download-btn{
+    margin: 10px 0;
+    cursor: pointer;
+    color: #121212;
+    background-color: #23E7C0;
+    font-weight: bolder;
+    font-size: 25px;
+    border: none;
+    border-radius: 10px;
+    height: 60px;
+    width: 250px;
+}
 
-    .number:focus{
-        transition: all 0.1s ease-in;
-        outline: 2px solid #23E7C0;
-    }
+.qr-code-container {
+    border-radius: 10px;
+    background: #DAF3EA;
+    height: 250px;
+    width: 250px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 
-    .number:not(:focus){
-        transition: all 0.1s ease-out;
-    }
+.input-container {
+    justify-content: center;
+    align-items: start;
+    display: flex;
+    flex-direction: row;
+}
 
-    .message{
-        resize: none;
-        font-family: 'Poppins';
-        outline: #FCFCFC;
-        border: none;
-        border-radius: 10px;
-        background-color: #DAF3EA;
-        padding: 15px;
-        font-size: 20px;
-        height: 200px;
-        width: 400px;
-    }
+.btn-download {
+    margin-top: 10px;
+}
 
-    .message:focus{
-        transition: all 0.1s ease-in;
-        outline: 2px solid #23E7C0;
-    }
+::placeholder {
+    color: #8f8f8f;
+}
 
-    .message:not(:focus){
-        transition: all 0.1s ease-out;
-    }
+.number {
+    margin: 20px 0;
+    font-family: 'Poppins';
+    outline: #FCFCFC;
+    border: none;
+    border-radius: 10px;
+    background-color: #DAF3EA;
+    padding: 15px;
+    font-size: 20px;
+    height: 60px;
+    width: 400px;
+}
 
-    .btn-go{
-        margin: 20px 0;
-        cursor: pointer;
-        color: #121212;
-        background-color: #23E7C0;
-        font-weight: bolder;
-        font-size: 25px;
-        border: none;
-        border-radius: 10px;
-        height: 60px;
-        width: 400px;
-    }
+.number:focus {
+    transition: all 0.1s ease-in;
+    outline: 2px solid #23E7C0;
+}
+
+.number:not(:focus) {
+    transition: all 0.1s ease-out;
+}
+
+.message {
+    resize: none;
+    font-family: 'Poppins';
+    outline: #FCFCFC;
+    border: none;
+    border-radius: 10px;
+    background-color: #DAF3EA;
+    padding: 15px;
+    font-size: 20px;
+    height: 200px;
+    width: 400px;
+}
+
+.message:focus {
+    transition: all 0.1s ease-in;
+    outline: 2px solid #23E7C0;
+}
+
+.message:not(:focus) {
+    transition: all 0.1s ease-out;
+}
+
+.btn-go {
+    margin: 20px 0;
+    cursor: pointer;
+    color: #121212;
+    background-color: #23E7C0;
+    font-weight: bolder;
+    font-size: 25px;
+    border: none;
+    border-radius: 10px;
+    height: 60px;
+    width: 400px;
+}
 
 </style>
 
